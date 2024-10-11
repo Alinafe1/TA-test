2b8deefdc227a9e19929204aa6e9d00871a7e996
Alinafe Phiri <alinafe.phiri@za.ey.com>
2024-10-11 08:52:59 +0200

    New script

---
+++ cars.R
@@ -0,0 +1,9 @@
+head(cars)
+cars[,3] = cars[,2]/cars[,1]
+colnames(cars)[3] = "Time"
+
+x <- cars[,1]
+y <- cars[,2]
+z <- cars[,3]
+
+plot_ly(x = ~x, y= ~y, z = ~z, type = "scatter3d", mode = "markers")
